c4d7a6ac840d4aecc3558877a71678182b2ce864
Rob Meades <rob.meades@u-blox.com>
2016-12-06 13:37:28 +0000

    Tested and with tidied up logging.

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 /* Entry point for UDP echo server.
  *
- * This code based:
- * https://gist.github.com/paulsmith/775764
+ * This code based on the TCP echo server also in this repo.
  */
  
 package main
@@ -11,7 +10,7 @@ import (
     "fmt"
     "os"
     "flag"
-    "bufio"
+    "log"
 )
 
 //--------------------------------------------------------------------
@@ -22,13 +21,10 @@ import (
 // Variables
 //--------------------------------------------------------------------
 
-// File handle
-var pFile *os.File
 var numPackets int
 
 // Command-line flags
-var pPort = flag.String ("p", "", "the port number to listen on.")
-var pFileName = flag.String ("f", "", "the file name to write the receive requests to.")
+var pPort = flag.String ("p", "", "the UDP port number to listen on.")
 var Usage = func() {
     fmt.Fprintf(os.Stderr, "\n%s: run the UDP echo server.  Usage:\n", os.Args[0])
         flag.PrintDefaults()
@@ -40,55 +36,48 @@ var Usage = func() {
 
 // Entry point
 func main() {
-    var err error
-    var line []byte
+    var readErr error
+    var numBytesIn int
+    var pRemoteUdpAddr *net.UDPAddr
+    line := make([]byte, 1024)
 
     // Deal with the command-line parameters
     flag.Parse()
     
+    // Set up logging
+    log.SetFlags(log.LstdFlags)
+    
     if *pPort != "" {
-        // Open the output file for append
-        if *pFileName != "" {
-            pFile, err = os.OpenFile(*pFileName, os.O_WRONLY | os.O_APPEND | os.O_CREATE, 0666)
-        }    
+        // Say what we're doing
+        fmt.Printf("Echoing UDP packets received on port %s.\n", *pPort)
         
-        if err == nil {        
-            // Say what we're doing
-            fmt.Printf("Echoing UDP packets received on port %s", *pPort)
-            if (pFile != nil) {
-                fmt.Printf(" and writing received packets to \"%s\"", pFile.Name())
-            }
-            fmt.Printf(".\n")
-            
-            // Set up the server
-            pLocalUdpAddr, err := net.ResolveUDPAddr("udp", ":" + *pPort)
+        // Set up the server
+        pLocalUdpAddr, err := net.ResolveUDPAddr("udp", ":" + *pPort)
+        if (err == nil) && (pLocalUdpAddr != nil) {
+            pServer, err := net.ListenUDP("udp", pLocalUdpAddr)
             if err == nil {
-                pServer, err := net.ListenUDP("udp", pLocalUdpAddr)
-                if err == nil {
-                    for numBytesIn, pRemoteUdpAddr, readErr := pServer.ReadFromUDP(line); readErr == nil; {
-                        if numBytesIn > 0 {
-                            fmt.Printf("%d: %v <-> %v.\n", numPackets, pLocalUdpAddr, pRemoteUdpAddr)                    
-                            numBytesOut, writeErr := pServer.WriteToUDP(line, pRemoteUdpAddr)
-                            if writeErr != nil {
-                                fmt.Printf("Error, only %d of %d byte(s) could be echoed (%s).\n", numBytesOut, numBytesIn, writeErr.Error())    
-                            }
-                            if pFile != nil {
-                                writer := bufio.NewWriter(pFile)
-                                fmt.Fprintf(writer, "%d: %v <-> %v \"%s\".\n", numPackets, pLocalUdpAddr, pRemoteUdpAddr, line)
-                            }
+                if err == nil {                    
+                    for numBytesIn, pRemoteUdpAddr, readErr = pServer.ReadFromUDP(line); (readErr == nil) && (numBytesIn > 0); numBytesIn, pRemoteUdpAddr, readErr = pServer.ReadFromUDP(line) {
+                        numBytesOut, writeErr := pServer.WriteToUDP(line[:numBytesIn], pRemoteUdpAddr)
+                        log.Printf("%d: %v <-> %v: %s\n", numPackets, pLocalUdpAddr, pRemoteUdpAddr, line[:numBytesIn])
+                        if writeErr != nil {
+                            log.Printf("Error, only %d of %d byte(s) could be echoed (%s).\n", numBytesOut, numBytesIn, writeErr.Error())    
                         }
                     }    
-                    fmt.Printf("Unable to read from port %v (%s).\n", pLocalUdpAddr)    
-                } else {                
-                    fmt.Printf("Couldn't start UDP server on port %s (%s).\n", *pPort, err.Error())
-                }            
+                    if readErr != nil {
+                        fmt.Printf("Error reading from port %v (%s).\n", pLocalUdpAddr, readErr.Error())
+                    } else {
+                        log.Printf("UDP read on port %v returned when it should not.\n", pLocalUdpAddr)    
+                    }
+                } else {
+                    fmt.Printf("Couldn't disable read deadline (%s).\n", err.Error())
+                }
             } else {
-                fmt.Printf("'%s' is not a valid UDP address (%s).\n", *pPort, err.Error())
+                fmt.Printf("Couldn't start UDP server on port %s (%s).\n", *pPort, err.Error())
             }            
         } else {
-            fmt.Printf("Couldn't open file %s (%s).\n", *pFileName, err.Error())
-            os.Exit(-1)
-        }
+            fmt.Printf("'%s' is not a valid UDP address (%s).\n", *pPort, err.Error())
+        }            
     } else {
         fmt.Printf("Must specify a port number.\n")
         flag.PrintDefaults()
